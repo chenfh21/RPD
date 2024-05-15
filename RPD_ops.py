5bf7ed7b84fffe2231daa387ad11a1299f43c7ef
chenfh21 <97225052+chenfh21@users.noreply.github.com>
2024-05-15 11:55:20 +0800

    Add files via upload

---
+++ RPD_ops.py
@@ -0,0 +1,222 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from typing import List
+
+
+class Conv2d(nn.Module):
+    def __init__(self, op, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1,
+                 bias=False):
+        super(Conv2d, self).__init__()
+        if in_channels % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        # if out_channels % groups != 0:
+        #     raise ValueError('out_channels must be divisible by groups')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        self.weight = nn.Parameter(torch.Tensor(out_channels, in_channels // groups, kernel_size, kernel_size))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+        self.op = op
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        if self.op is None:
+            return F.conv2d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        else:
+            return self.op(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+
+## cd, ad, rd convolutions
+## theta could be used to control the vanilla conv components
+## theta = 0 reduces the function to vanilla conv, theta = 1 reduces the function to pure pdc
+def createConvFunc(op_type, theta=0.7):
+    assert op_type in ['cv', 'cd', 'ad', 'rd', 'erd'], 'unknown op type: %s' % str(op_type)
+    if op_type == 'cv':
+        return F.conv2d
+
+    assert theta >= 0 and theta <= 1.0, 'theta should be within (0, 1]'
+
+    if op_type == 'cd':
+        def func(x, weights, bias=None, stride=1, padding=0, dilation=1, groups=1):
+            assert dilation in [1, 2], 'dilation for cd_conv should be in 1 or 2'
+            assert weights.size(2) == 3 and weights.size(3) == 3, 'kernel size for cd_conv should be 3x3'
+            assert padding == dilation, 'padding for cd_conv set wrong'
+
+            weights_c = weights.sum(dim=[2, 3], keepdim=True) * theta
+            yc = F.conv2d(x, weights_c, stride=stride, padding=0, groups=groups)
+            y = F.conv2d(x, weights, bias, stride=stride, padding=padding, dilation=dilation, groups=groups)
+            return y - yc
+
+        return func
+    elif op_type == 'ad':
+        def func(x, weights, bias=None, stride=1, padding=0, dilation=1, groups=1):
+            assert dilation in [1, 2], 'dilation for ad_conv should be in 1 or 2'
+            assert weights.size(2) == 3 and weights.size(3) == 3, 'kernel size for ad_conv should be 3x3'
+            assert padding == dilation, 'padding for ad_conv set wrong'
+
+            shape = weights.shape
+            weights = weights.view(shape[0], shape[1], -1)
+            weights_conv = (weights - theta * weights[:, :, [3, 0, 1, 6, 4, 2, 7, 8, 5]]).view(shape)  # clock-wise
+            y = F.conv2d(x, weights_conv, bias, stride=stride, padding=padding, dilation=dilation, groups=groups)
+            return y
+
+        return func
+    elif op_type == 'rd':
+        def func(x, weights, bias=None, stride=1, padding=0, dilation=1, groups=1):
+            assert dilation in [1, 2], 'dilation for rd_conv should be in 1 or 2'
+            assert weights.size(2) == 3 and weights.size(3) == 3, 'kernel size for rd_conv should be 3x3'
+            padding = 2 * dilation
+
+            shape = weights.shape
+            if weights.is_cuda:
+                buffer = torch.tensor([0], dtype=torch.float32, device='cuda').repeat(shape[0], shape[1], 5 * 5)
+            else:
+                buffer = torch.zeros(shape[0], shape[1], 5 * 5)
+            weights = weights.view(shape[0], shape[1], -1)
+            buffer[:, :, [0, 2, 4, 10, 14, 20, 22, 24]] = weights[:, :, 1:]
+            buffer[:, :, [6, 7, 8, 11, 13, 16, 17, 18]] = -weights[:, :, 1:] * theta
+            buffer[:, :, 12] = weights[:, :, 0] * (1 - theta)
+            buffer = buffer.view(shape[0], shape[1], 5, 5)
+            y = F.conv2d(x, buffer, bias, stride=stride, padding=padding, dilation=dilation, groups=groups)
+            return y
+
+        return func
+
+    elif op_type == 'erd':
+        def func(x, weights, bias=None, stride=1, padding=0, dilation=1, groups=1):
+            assert dilation in [1, 2]
+            assert weights.size(2) == 3 and weights.size(3) == 3
+
+            weights_conv = (weights - weights.sum(dim=[2, 3], keepdim=True)) * theta
+            y = F.conv2d(x, weights_conv, bias, stride=stride, padding=2, dilation=2, groups=groups)
+            return y
+
+        return func
+
+    else:
+        print('impossible to be here unless you force that')
+        return None
+
+
+def convert_pdc(op_type, weight):
+    if op_type == 'cv':
+        return weight
+    elif op_type == 'cd':
+        shape = weight.shape
+        weight_c = weight.sum(dim=[2, 3])
+        weight = weight.view(shape[0], shape[1], -1)
+        weight[:, :, 4] = weight[:, :, 4] - weight_c
+        weight = weight.view(shape)
+        return weight
+    elif op_type == 'ad':
+        shape = weight.shape
+        weight = weight.view(shape[0], shape[1], -1)
+        weight_conv = (weight - weight[:, :, [3, 0, 1, 6, 4, 2, 7, 8, 5]]).view(shape)
+        return weight_conv
+    elif op_type == 'rd':
+        shape = weight.shape
+        buffer = torch.zeros(shape[0], shape[1], 5 * 5, device=weight.device)
+        weight = weight.view(shape[0], shape[1], -1)
+        buffer[:, :, [0, 2, 4, 10, 14, 20, 22, 24]] = weight[:, :, 1:]
+        buffer[:, :, [6, 7, 8, 11, 13, 16, 17, 18]] = -weight[:, :, 1:]
+        buffer = buffer.view(shape[0], shape[1], 5, 5)
+        return buffer
+    elif op_type == 'erd':
+        shape = weight.shape
+        weight_c = weight.sum(dim=[2, 3])
+        weight = weight.view(shape[0], shape[1], -1)
+        weight[:, :, 4] = weight[:, :, 4] - weight_c
+        weight = weight.view(shape)
+        return weight
+
+    raise ValueError("wrong op {}".format(str(op_type)))
+
+
+blocks_config = {
+    'cv': {
+        'block_opd': 'cv'
+    },
+
+    'cdc': {
+        'block_opd1': 'cv',
+        'block_opd2': 'cv',
+        'block_opd3': 'cv',
+        'block_opc': 'cd'
+    },
+
+    'cadc': {
+        'block_opd1': 'cv',
+        'block_opc': 'cd',
+        'block_opd2': 'cv',
+        'block_opa': 'ad'
+    },
+
+    'pdc': {
+        'block_opd': 'cv',
+        'block_opc': 'cd',
+        'block_opa': 'ad',
+        'block_opr': 'rd',
+    },
+
+    'epdc': {
+        'block_opd': 'cv',
+        'block_opc': 'cd',
+        'block_opa': 'ad',
+        'block_opr': 'erd',
+    }
+}
+
+
+def config_block(block: str) -> List:
+    block_options = list(blocks_config.keys())
+    assert block in block_options, f'unrecognized block_ops, please choose from {block_options}'
+
+    cv_list = [createConvFunc(v, theta=1) for k, v in blocks_config[block].items()]
+
+    return cv_list
+
+
+# 生成函数/方法操作
+def config_block_converted(block: str) -> List:
+    block_options = list(blocks_config.keys())
+    assert block in block_options, f'unrecognized block_ops, please choose from {block_options}'
+
+    cv_list = [op for op_type, op in blocks_config[block].items()]
+
+    return cv_list
+
+
+def convert_block(state_dict, config):  # state_dict -> block_config
+    cv_ops = config_block_converted(config)  # config -> e.g. 'pdc'.操作选项
+    new_dict = {}
+
+    for pname, p in state_dict.items():
+        if pname.endswith('rbr_conv.0.conv.weight'):
+            new_dict[pname] = convert_pdc(cv_ops[0], p)
+        elif pname.endswith('rbr_conv.1.conv.weight') and p.shape[2] != 1:
+            new_dict[pname] = convert_pdc(cv_ops[1], p)
+        elif pname.endswith('rbr_conv.2.conv.weight') and p.shape[2] != 1:
+            new_dict[pname] = convert_pdc(cv_ops[2], p)
+        elif pname.endswith('rbr_conv.3.conv.weight') and p.shape[2] != 1:
+            new_dict[pname] = convert_pdc(cv_ops[3], p)
+        else:
+            new_dict[pname] = p
+
+    return new_dict
---
+++ unet.py
@@ -0,0 +1,209 @@
+import torch
+from torch import nn
+import copy
+from pdc_modules.RPD_Module import *
+
+
+def conv_bn_act(in_channels, out_channels, kernel_size, stride, padding, groups):
+    if padding is None:
+        padding = kernel_size // 2
+    mod_list = nn.Sequential()
+    mod_list.add_module('conv', nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                          stride=stride, padding=padding, groups=groups, bias=False))
+    mod_list.add_module('bn', nn.BatchNorm2d(num_features=out_channels))
+    mod_list.add_module('act', nn.ReLU())
+    return mod_list
+
+
+def fuse_bn(conv, bn):
+    kernel = conv.weight
+    running_mean = bn.running_mean
+    running_var = bn.running_var
+    gamma = bn.weight
+    beta = bn.bias
+    eps = bn.eps
+    std = (running_var + eps).sqrt()
+    t = (gamma / std).reshape(-1, 1, 1, 1)
+    return kernel * t, beta - running_mean * gamma / std
+
+
+class RepConvbn(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, groups=1, deploy=False):
+        super(RepConvbn, self).__init__()
+
+        self.deploy = deploy
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.activation = nn.ReLU(inplace=True)
+
+        padding = kernel_size // 2
+
+        if deploy:
+            self.cba_rep = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups, bias=True)
+        else:
+            self.cba_ori = conv_bn_act(in_channels, out_channels, kernel_size, stride, padding, groups)
+
+    def forward(self, inputs):
+        if hasattr(self, 'cba_rep'):
+            out = self.activation(self.cba_rep(inputs))
+        else:
+            out = self.cba_ori(inputs)
+        return out
+
+    def get_equivalent_kernel_bias(self):
+        eq_k, eq_b = fuse_bn(self.cba_ori.conv, self.cba_ori.bn)
+        return eq_k, eq_b
+
+    def switch_to_deploy(self):
+        eq_k, eq_b = self.get_equivalent_kernel_bias()
+        self.cba_rep = nn.Conv2d(in_channels=self.cba_ori.conv.in_channels,
+                                 out_channels=self.cba_ori.conv.out_channels,
+                                 kernel_size=self.cba_ori.conv.kernel_size,
+                                 stride=self.cba_ori.conv.stride,
+                                 padding=self.cba_ori.conv.padding,
+                                 groups=self.cba_ori.conv.groups)
+        self.cba_rep.weight.data = eq_k
+        self.cba_rep.bias.data = eq_b
+        self.__delattr__('cba_ori')
+
+
+class DoubleConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, groups=1, deploy=False, mid_channels=None):
+        if mid_channels is None:
+            mid_channels = out_channels
+        super(DoubleConv, self).__init__(
+            RepConvbn(in_channels, mid_channels, kernel_size, stride=stride, groups=groups, deploy=deploy),
+            RepConvbn(mid_channels, out_channels, kernel_size, stride=stride, groups=groups, deploy=deploy)
+        )
+
+
+class DoubleConv_down(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, deploy=False, use_se=False, convert=False):
+        if mid_channels is None:
+            mid_channels = out_channels
+        super(DoubleConv_down, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.mid_channels = mid_channels
+
+        self.Conv1 = self._make_stage(in_channels, mid_channels, deploy, use_se, convert)
+        self.Conv2 = self._make_stage(mid_channels, out_channels, deploy, use_se, convert)
+
+    def forward(self, input):
+        x = self.Conv1(input)
+        x = self.Conv2(x)
+
+        return x
+
+    def _make_stage(self, in_channels, out_channels, deploy, use_se, convert):
+        rpd_layers = []
+        rpd_layers.append(RPD('epdc', in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1,
+                              groups=in_channels, bias=True, deploy=deploy, use_se=use_se, convert=convert,
+                              num_conv_branches=1))
+        rpd_layers.append(RPD('cv', in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1,
+                              groups=1, bias=True, deploy=deploy, use_se=use_se, convert=convert, num_conv_branches=4))
+
+        return nn.Sequential(*rpd_layers)
+
+
+class Down(nn.Sequential):
+    def __init__(self, in_channels, out_channels, deploy=False, use_se=False, convert=False):
+        super(Down, self).__init__(
+            nn.MaxPool2d(2, stride=2),
+            DoubleConv_down(in_channels, out_channels, deploy=deploy, use_se=use_se, convert=convert)
+        )
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True, deploy=False, use_se=False, convert=False):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv_down(in_channels, out_channels, in_channels // 2, deploy=deploy, use_se=use_se,
+                                        convert=convert)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv_down(in_channels, out_channels, deploy=deploy, use_se=use_se, convert=convert)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # [N, C, H, W] 下面几行代码的作用是保障输入图像是16的整数倍，如果不是会存在下采样向下取整后在拼接时shape不一致的问题
+        diff_y = x2.size()[2] - x1.size()[2]
+        diff_x = x2.size()[3] - x1.size()[3]
+
+        # padding_left, padding_right, padding_top, padding_bottom
+        x1 = nn.functional.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+                                    diff_y // 2, diff_y - diff_y // 2])
+
+        x = torch.cat([x1, x2], dim=1)
+        x = self.conv(x)
+
+        return x
+
+
+class OutConv(nn.Sequential):
+    def __init__(self, in_channels, num_classes):
+        super(OutConv, self).__init__(
+            nn.Conv2d(in_channels, num_classes, kernel_size=1)
+        )
+
+
+class UNet(nn.Module):
+    def __init__(self,
+                 num_classes: int,
+                 in_channels: int = 3,
+                 bilinear: bool = True,
+                 base_c: int = 16,
+                 deploy=True,
+                 use_se=False,
+                 convert=False):
+        super(UNet, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.bilinear = bilinear
+
+        self.in_conv = DoubleConv(in_channels, base_c, deploy=deploy)
+        self.down1 = Down(base_c, base_c * 2, deploy=deploy, use_se=use_se, convert=convert)
+        self.down2 = Down(base_c * 2, base_c * 4, deploy=deploy, use_se=use_se, convert=convert)
+        self.down3 = Down(base_c * 4, base_c * 8, deploy=deploy, use_se=use_se, convert=convert)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(base_c * 8, base_c * 16 // factor, deploy=deploy, use_se=use_se, convert=convert)
+        self.up1 = Up(base_c * 16, base_c * 8 // factor, bilinear, deploy=deploy, use_se=use_se, convert=convert)
+        self.up2 = Up(base_c * 8, base_c * 4 // factor, bilinear, deploy=deploy, use_se=use_se, convert=convert)
+        self.up3 = Up(base_c * 4, base_c * 2 // factor, bilinear, deploy=deploy, use_se=use_se, convert=convert)
+        self.up4 = Up(base_c * 2, base_c, bilinear, deploy=deploy, use_se=use_se, convert=convert)
+        self.out_conv = OutConv(base_c, num_classes)
+
+    def forward(self, x):
+        x1 = self.in_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        return self.out_conv(x)
+
+# if __name__ == '__main__':
+#     model = UNet(in_channels=3, num_classes=3)
+#     print(model)
+#
+#     model.eval()
+#     print('------------------- training-time model -------------')
+#     # print(model)
+#     x = torch.randn(2, 3, 512, 512)
+#     origin_y = model(x)
+#     print(origin_y.shape)
+
+# model.switch_to_deploy()
+# print('------------------- after re-param -------------')
+# print(model)
+# reparam_y = model(x)
+# print('------------------- the difference is ------------------------')
+# print((origin_y - reparam_y).abs().sum())
