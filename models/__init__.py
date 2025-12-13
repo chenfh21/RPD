d6110a397d56b64c3550f1361d7033b96e781b64
chenfh21 <97225052+chenfh21@users.noreply.github.com>
2025-12-13 21:01:50 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,19 @@
-from .deeplabv3plus_model import *
+from .deeplab.modeling import deeplabv3plus_resnet50
+from .ori_unet import UNet
 from .RPDNet import RPDNet
 from .erfnet import ERFNetModel
 from .segnext import SegNext
 from .segformer import SegFormer
+from .efficientvit import EfficientViTSeg
 from .model_multimetrics import *
+from .fgnet import FGNet
 from .losses import *
+from .RPD_ops import *
 
 
 def get_backbone(cfg: Dict) -> nn.Module:
     num_classes = cfg['backbone']['num_classes']
     pretrained = cfg['backbone']['pretrained']
-    deploy = cfg['backbone']['deploy']
-    convert = cfg['backbone']['convert']
 
     if cfg['backbone']['name'] == 'deeplabv3plus_resnet50':
         return deeplabv3plus_resnet50(output_stride=16, num_classes=num_classes, pretrained_backbone=pretrained)
@@ -19,18 +21,26 @@ def get_backbone(cfg: Dict) -> nn.Module:
     if cfg['backbone']['name'] == 'erfnet':
         return ERFNetModel(num_classes, pretrained=pretrained)
 
-    if cfg['backbone']['name'] == 'baseline':
-        return UNet(num_classes)
-
-    if cfg['backbone']['name'] == 'RPDNet':
-        return RPDNet(num_classes, deploy=deploy, convert=convert)
-        # return RPDNet(num_classes)
-
     if cfg['backbone']['name'] == 'segnext':
         return SegNext(num_classes)
 
     if cfg['backbone']['name'] == 'segformer':
         return SegFormer(num_classes)
 
+    if cfg['backbone']['name'] == 'efficientvitseg':
+        return EfficientViTSeg(num_classes)
+
+    if cfg['backbone']['name'] == 'fgnet':
+        return FGNet(num_classes)
+
+    if cfg['backbone']['name'] == 'baseline':
+        return UNet(num_classes)
+
+    if cfg['backbone']['name'] == 'RPDNet':
+        deploy = cfg['backbone']['deploy']
+        convert = cfg['backbone']['convert']
+        return RPDNet(num_classes, deploy=deploy, convert=convert)
+
     raise ValueError('The requested backbone is not supported.')
+
 
